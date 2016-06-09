72f4bd15abaa465cf5723781d7de2f9af9df3309
webimpress <contact@webimpress.com>
2016-06-09 22:44:22 +0100

    DiAbstractServiceFactoryFactory extends corresponding class from zend-servicemanager-di (class marked as deprecated in zend-mvc, removed with 3.0 release)

--- src/Service/DiAbstractServiceFactoryFactory.php
+++ src/Service/DiAbstractServiceFactoryFactory.php
@@ -9,43 +9,10 @@
 
 namespace Zend\Mvc\Service;
 
-use Interop\Container\ContainerInterface;
-use Zend\ServiceManager\Di\DiAbstractServiceFactory;
-use Zend\ServiceManager\FactoryInterface;
-use Zend\ServiceManager\ServiceLocatorInterface;
-use Zend\ServiceManager\ServiceManager;
-
-class DiAbstractServiceFactoryFactory implements FactoryInterface
+/**
+ * @deprecated please use the {@see \Zend\ServiceManager\Di\DiAbstractServiceFactoryFactory} class instead:
+ *             this class will be removed in release 3.0
+ */
+class DiAbstractServiceFactoryFactory extends \Zend\ServiceManager\Di\DiAbstractServiceFactoryFactory
 {
-    /**
-     * Class responsible for instantiating a DiAbstractServiceFactory
-     *
-     * @param ContainerInterface $container
-     * @param string $name
-     * @param null|array $options
-     * @return DiAbstractServiceFactory
-     */
-    public function __invoke(ContainerInterface $container, $name, array $options = null)
-    {
-        $factory = new DiAbstractServiceFactory($container->get('Di'), DiAbstractServiceFactory::USE_SL_BEFORE_DI);
-
-        if ($container instanceof ServiceManager) {
-            $container->addAbstractFactory($factory, false);
-        }
-
-        return $factory;
-    }
-
-    /**
-     * Create and return DiAbstractServiceFactory instance
-     *
-     * For use with zend-servicemanager v2; proxies to __invoke().
-     *
-     * @param ServiceLocatorInterface $container
-     * @return DiAbstractServiceFactory
-     */
-    public function createService(ServiceLocatorInterface $container)
-    {
-        return $this($container, DiAbstractServiceFactory::class);
-    }
 }
