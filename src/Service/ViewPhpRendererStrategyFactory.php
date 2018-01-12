d0d11322c8d7a05c491b647e9e514a4dd1abbbb5
Aleksei Khudiakov <aleksey@xerkus.pro>
2018-01-13 06:31:39 +1000

    Add deprecated factories in Service namespace for BC

---
+++ src/Service/ViewPhpRendererStrategyFactory.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/zend-mvc for the canonical source repository
+ * @copyright Copyright (c) 2005-2018 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   https://github.com/zendframework/zend-mvc/blob/master/LICENSE.md New BSD License
+ */
+
+namespace Zend\Mvc\Service;
+
+use Zend\Mvc\Container;
+
+/**
+ * @deprecated Since 3.2
+ */
+class ViewPhpRendererStrategyFactory extends Container\ViewPhpRendererStrategyFactory
+{
+}
