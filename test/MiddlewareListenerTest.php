b8fff0c890626f087e74b5517832714d9b81ef9d
Cvetomir Lazarov <cvetomir.lazarov@gmail.com>
2018-09-12 15:21:54 +0300

    Update header doc block comment

--- test/MiddlewareListenerTest.php
+++ test/MiddlewareListenerTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @link      http://github.com/zendframework/zend-mvc for the canonical source repository
+ * @see       https://github.com/zendframework/zend-mvc for the canonical source repository
  * @copyright Copyright (c) 2005-2018 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   https://github.com/zendframework/zend-mvc/blob/master/LICENSE.md New BSD License
  */
