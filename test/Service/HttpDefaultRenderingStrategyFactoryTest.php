498163d81a60f95c31340ac19153cb95aa655113
Thomas Vargiu <tvargiu@gmail.com>
2018-10-15 19:06:59 +0200

    Added HttpDefaultRenderingStrategyFactoryTest

---
+++ test/Service/HttpDefaultRenderingStrategyFactoryTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace ZendTest\Mvc\Service;
+
+use Interop\Container\ContainerInterface;
+use Zend\Mvc\Service\HttpDefaultRenderingStrategyFactory;
+use PHPUnit\Framework\TestCase;
+use PHPUnit_Framework_MockObject_MockObject as MockObject;
+use Zend\Mvc\View\Http\DefaultRenderingStrategy;
+use Zend\View\View;
+
+class HttpDefaultRenderingStrategyFactoryTest extends TestCase
+{
+    /**
+     * @var ContainerInterface|MockObject
+     */
+    protected $serviceLocator;
+
+    public function setUp()
+    {
+        $this->serviceLocator = $this->prophesize(ContainerInterface::class);
+    }
+
+    public function testInvoke()
+    {
+        $factory = new HttpDefaultRenderingStrategyFactory();
+
+        $view = $this->prophesize(View::class);
+        $this->serviceLocator->get(View::class)
+            ->willReturn($view->reveal());
+
+        $this->serviceLocator->has('config')->willReturn(true);
+        $this->serviceLocator->get('config')
+            ->willReturn([
+                'view_manager' => [
+                    'layout' => 'foo',
+                ],
+            ]);
+
+        $instance = $factory($this->serviceLocator->reveal(), 'foo');
+
+        $this->assertInstanceOf(DefaultRenderingStrategy::class, $instance);
+        $this->assertSame('foo', $instance->getLayoutTemplate());
+    }
+
+    public function testInvokeWithEmptyObject()
+    {
+        $factory = new HttpDefaultRenderingStrategyFactory();
+
+        $view = $this->prophesize(View::class);
+        $this->serviceLocator->get(View::class)
+            ->willReturn($view->reveal());
+
+        $this->serviceLocator->has('config')->willReturn(true);
+        $this->serviceLocator->get('config')
+            ->willReturn([]);
+
+        $instance = $factory($this->serviceLocator->reveal(), 'foo');
+
+        $this->assertInstanceOf(DefaultRenderingStrategy::class, $instance);
+        $this->assertSame('layout/layout', $instance->getLayoutTemplate());
+    }
+}
